e0c609231cbaa7d175c209774a606b26880d239d
Velinov <124807884+TsvetanVelinov@users.noreply.github.com>
2023-03-11 12:09:38 +0200

    Lap6.2
    While loop

---
+++ Lap6.2.js
@@ -0,0 +1,24 @@
+function passwordTask (input){
+
+    let userName = input[0];
+    let password = input[1];
+
+    let data = input[2];
+    let index = 3;
+
+    while( data !== password){
+        data=input[index];
+        index++;
+    }
+    console.log(`Welcome ${userName}`);
+
+
+
+}
+
+
+passwordTask (["Nakov",
+"1234",
+"Pass",
+"1324",
+"1234"])
